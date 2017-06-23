687f353ae61cde5177680ede17fa4f49f68f813c
Sergio Anastacio Angulo Rosales <senao28@icloud.com>
2017-06-23 02:05:43 -0500

    update
    adasd

---
+++ home.php
@@ -0,0 +1,106 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="base_home.css">
+    </head>
+    <body>
+        <div id="warper">
+              <!--top-->
+              <div id="header">
+                   <div class="posfixed">
+                         <div id="TA"><!--Logo>SearchSection>AccountName-->
+                              <figure id="Logosec">
+                                  <img id="logo" src="img/logo/uniat.png">
+                              </figure>
+                              <!--Search-->
+                              <div id="searchbar">
+                                  <form id="SearchBrowser">
+                                      <input type="text" placeholder="Search in our store" id="TextArea">
+                                      <input type="submit" value="Search" id="SubmitArea">
+                                  </form>
+                              </div>
+                              <!--ACC-->
+                              <div id="accountname">
+                                   <div id="NameandPick">
+                                   
+                                   <figure id="ProfilePic">
+                                       <a href="#"><img src="img/profilepicks/Uzumaki.jpg"></a>
+                                   </figure>
+                                   
+                                   <div id="username">
+                                       <p>Uzumaki</p>
+                                       </div>
+
+                                    </div>
+                                    <div id="opcions">
+                                        <ul>
+                                            <li><a href="#"class="links">Log Out</a></li>
+                                            <li><a href="#"class="links">MyCart</a></li>
+                                            <li><a href="#"class="links">MyAccount</a></li>
+                                            
+                                            
+                                           
+                                        </ul>
+                                        
+                                    </div>
+                              </div>
+                         </div>
+
+                   </div>
+                   <!--NavBar-->
+                   <div id="nabvar">
+                       <div class="posfixed" >
+                         <div id="TB"><!--Navbar-->
+                              
+                              <ul id="navfix">
+                                <li><a href="#"><br><p>Products</p></a></li>
+                                <li><a><br><p>Sale</p></a></li>
+                                <li><a><br><p>Stocklist</p></a></li>
+                                <li><a><br><p>Gaming</p></a></li>
+                                <li><a><br><p>Stores</p></a></li>
+                                
+                              </ul>
+
+                         </div>
+                       </div>
+                   </div>
+              </div>
+              <!--mid-->
+              <div class="posfixed" id="mid">
+                <!--Top panel-->
+                <div id="map"></div><!--SiteMap > someinfo extra-->
+               
+               <div id="grid">
+                   <!--Lpanel-->
+                   <div id="Lpanel">
+
+                   </div>
+                   <!--Rpanel-->
+                   <div id="Rpanel">
+                       <div id="Info">
+                          <h2>Titulodelarea</h2>
+                           <div>
+                          <p>386</p>
+                          <h4>Productos Encontrados:</h4>
+                          </div>
+                       </div>
+                       <div id="Settings">
+                         
+                       </div>
+                       <div id="inlineGrid">
+                             <div>
+                                 
+                             </div>
+                       </div>
+                   </div>
+               </div>
+              </div>
+              <!--bot-->
+              <div id="footer" >
+                   <div class="posfixed" id="bot">
+
+                   </div>
+              </div>
+        </div>
+    </body>
+</html>
