457d556fce4e9c8c803efc63724bf8383f01b09b
Sergio Anastacio Angulo Rosales <senao28@icloud.com>
2017-06-28 20:15:21 -0500

    Update Database

--- DB.sql
+++ DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-06-2017 a las 01:48:13
+-- Tiempo de generación: 29-06-2017 a las 01:14:01
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -17,32 +17,54 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_web_4_test`
+-- Base de datos: `u720179037_3exam`
 --
-CREATE DATABASE IF NOT EXISTS `db_web_4_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `db_web_4_test`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `page`
+-- Estructura de tabla para la tabla `content_data`
 --
 
-CREATE TABLE `page` (
+CREATE TABLE `content_data` (
+  `id` int(100) UNSIGNED NOT NULL,
+  `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `subbaner` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `extra` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Tabla de contenido en el cuerpo de la pagina';
+
+--
+-- Volcado de datos para la tabla `content_data`
+--
+
+INSERT INTO `content_data` (`id`, `title`, `description`, `subbaner`, `extra`) VALUES
+(1, 'TestTitleOFPage', 'this desc is a desc of this page ', 'this is a subbanering who contain the name of this pic ', 'content extra for varius propuces'),
+(2, 'TestTitleOFPage', 'this desc is a desc of this page ', 'this is a subbanering who contain the name of this pic ', 'content extra for varius propuces');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `page_data`
+--
+
+CREATE TABLE `page_data` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(64) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `page`
+-- Volcado de datos para la tabla `page_data`
 --
 
-INSERT INTO `page` (`id`, `title`) VALUES
+INSERT INTO `page_data` (`id`, `title`) VALUES
 (1, 'Home'),
 (2, 'Aboutus'),
 (3, 'contact'),
 (4, 'Login'),
-(5, 'Register');
+(5, 'Register'),
+(7, 'test'),
+(9, 'test');
 
 -- --------------------------------------------------------
 
@@ -83,28 +105,35 @@ CREATE TABLE `users_data` (
   `nombre` varchar(24) NOT NULL,
   `apellido_paterno` varchar(12) NOT NULL,
   `apellido_materno` varchar(12) NOT NULL,
-  `telefono` varchar(15) NOT NULL
+  `telefono` varchar(15) NOT NULL,
+  `profilepic` varchar(60) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='columna telefono  es un numero.';
 
 --
 -- Volcado de datos para la tabla `users_data`
 --
 
-INSERT INTO `users_data` (`id`, `email`, `nombre`, `apellido_paterno`, `apellido_materno`, `telefono`) VALUES
-(1, 'senao28@prodigy.net.mx', 'Tachool', 'angulo', 'rosales', '3111646067'),
-(2, 'senao28@icloud.com', 'Sergio', 'angulo', 'rosales', '3111646067'),
-(3, 'tachoo@tachoo.xyz', 'Uriel', 'angulo', 'rosales', '3111646067'),
-(4, 'tachoouniat@hotmail.com', 'Rosario', 'rosales', 'quinones', '3111646067'),
-(5, 'huerta98@prodigy.net.mx', 'Sergio', 'angulo', 'sagrero', '3111646067');
+INSERT INTO `users_data` (`id`, `email`, `nombre`, `apellido_paterno`, `apellido_materno`, `telefono`, `profilepic`) VALUES
+(1, 'senao28@prodigy.net.mx', 'Tachool', 'angulo', 'rosales', '3111646067', 'kakashi.jpg'),
+(2, 'senao28@icloud.com', 'Sergio', 'angulo', 'rosales', '3111646067', 'obito.jpg'),
+(3, 'tachoo@tachoo.xyz', 'Uriel', 'angulo', 'rosales', '3111646067', 'hinata.jpg'),
+(4, 'tachoouniat@hotmail.com', 'Rosario', 'rosales', 'quinones', '3111646067', 'boruto.jpg'),
+(5, 'huerta98@prodigy.net.mx', 'Sergio', 'angulo', 'sagrero', '3111646067', 'uzumaki.jpg');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `page`
+-- Indices de la tabla `content_data`
 --
-ALTER TABLE `page`
+ALTER TABLE `content_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `page_data`
+--
+ALTER TABLE `page_data`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -124,10 +153,15 @@ ALTER TABLE `users_data`
 --
 
 --
--- AUTO_INCREMENT de la tabla `page`
+-- AUTO_INCREMENT de la tabla `content_data`
 --
-ALTER TABLE `page`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `content_data`
+  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `page_data`
+--
+ALTER TABLE `page_data`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT de la tabla `password_data`
 --
